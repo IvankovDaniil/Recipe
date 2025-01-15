e3423dc0b65c19750d534556ce61caf72b9a61ca
Ivankov Daniil <Ivankovdaniil04@gmail.com>
2025-01-15 13:08:22 +0700

    Initial Commit

---
+++ Recipe/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Recipe
+//
+//  Created by Даниил Иваньков on 15.01.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
