68728669ae922405881eda3541596e0ec4c4f20e
Ivankov Daniil <Ivankovdaniil04@gmail.com>
2025-01-21 15:42:42 +0700

    Added RegisterView

--- Recipe/Model/UserModel.swift
+++ Recipe/Model/UserModel.swift
@@ -3,6 +3,7 @@ import Foundation
 struct UserModel: Identifiable, Decodable {
     var id: UUID = UUID()
     var name: String
+    var password: String
     var surname: String
     var email: String
     var avatarURL: String?
--- Recipe/UI/Screen/SettingsScreen.swift
+++ Recipe/UI/Screen/SettingsScreen.swift
@@ -23,13 +23,14 @@ struct SettingsScreen: View {
 }
 
 private struct NonRegisterView: View {
-    @State private var isPresented: Bool = false
+    @State private var isPresented: Bool = true
     
     var body: some View {
         VStack(spacing: 0) {
             Text("Вы не авторизованы")
             Text("Для того, что бы у вас была возможность отмечать рецепты и получать информацию на почту, зарегистрируйтесь")
                 .padding([.top, .bottom], 15)
+                .multilineTextAlignment(.center)
             
             Button {
                 isPresented = true
@@ -39,7 +40,7 @@ private struct NonRegisterView: View {
                     .blueRoundedBorder()
             }
             .sheet(isPresented: $isPresented, content: {
-                RegisterView()
+                RegisterView(action: { isPresented = false })
             })
             
 
@@ -49,10 +50,63 @@ private struct NonRegisterView: View {
 }
 
 private struct RegisterView: View {
+    @State private var name: String = ""
+    @State private var surname: String = ""
+    @State private var password: String = ""
+    @State private var email: String = ""
+    @State private var isValid: Bool = true
+    
+    let action: () -> Void
     var body: some View {
-        HStack(spacing: 0) {
-            Text("REEEGEEGEG")
+        NavigationStack{
+            ScrollView {
+                VStack(spacing: 0) {
+                    TextField("Введите имя", text: $name)
+                        .padding(.top, 15)
+                    TextField("Введите фамилию", text: $surname)
+                        .padding(.top, 15)
+                    TextField("Введите электронную почту", text: $email)
+                        .keyboardType(.emailAddress)
+                        .border(.clear)
+                        .padding(.top, 15)
+                    SecureField("Введите пароль", text: $password)
+                        .padding(.top, 15)
+                    
+                    Button {
+                        //
+                    } label: {
+                        Text("Регистрация")
+                            .padding(15)
+                            .blueRoundedBorder()
+                    }
+                    .padding(.top, 20)
+                }
+                .autocorrectionDisabled(true)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+                .padding(15)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            action()
+                        } label: {
+                            Text("Закрыть")
+                        }
+
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            action()
+                        } label: {
+                            Text("Сохранить")
+                        }
+
+                    }
+                }
+                .navigationTitle("Регистрация")
+            }
         }
+        
     }
 }
 
